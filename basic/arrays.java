fe287b9ca7ba1e6acd9bb1a80aee7190c768de6a
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-15 11:42:52 +0000

    Create arrays.java

---
+++ basic/arrays.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Arrays {
+
+	 public static void main(String[] args) {
+		    int[] numbers = {20,24,5,2,6,7,12,435,98,111};
+		    System.out.println(numbers[0]);
+		  }
+		}
