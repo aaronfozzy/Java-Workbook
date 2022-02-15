06a3f46fb8ac8070c67f770e5934cd571db326bc
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-15 11:45:05 +0000

    Create iterationarrays.java

---
+++ basic/iterationarrays.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class IterationArrays {
+
+	 public static void main(String[] args) {
+		    int[] numbers = {20,24,5,2,6,7,12,435,98,111};
+		    for (int i = 0; i < numbers.length; i++)
+		    System.out.println(numbers[i]);
+		  }
+		}
