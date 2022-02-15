fe61c7e79df5a78df946bcafd44ba0784436a1ff
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-15 11:46:13 +0000

    Create parameters.java

---
+++ basic/parameters.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class Parameters {
+	
+	static void myMethod(String name) {
+	    System.out.println(name + " Foster");
+	  }
+
+	  public static void main(String[] args) {
+	    myMethod("Liam");
+	    myMethod("Jenny");
+	    myMethod("Anja");
+	  }
+	}
+	// Liam Foster
+	// Jenny Foster
+	// Anja Foster
