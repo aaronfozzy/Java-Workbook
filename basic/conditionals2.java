ad87ade0005badfda12c6a20b3dde2b1c136ab32
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-15 11:44:14 +0000

    Create conditionals2.java

---
+++ basic/conditionals2.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class Conditionals2 {
+
+	  public static void main(String[] args) {
+		  int x = 20;
+		  int y = 0;
+		  double answer1 = x+y;
+		  
+		  
+		  
+		  if (x == 0) {
+		    System.out.println(y);
+		  }
+		  else if (y == 0) {
+			    System.out.println(x);
+			  }
+		    else
+		    {
+		    	System.out.println(answer1);
+		    }
+		  }
+	  }
+	
