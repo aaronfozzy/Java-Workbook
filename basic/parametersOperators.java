402e22f6798638b6f66af46c8dde9efb81e510b0
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-15 11:46:34 +0000

    Create parametersOperators.java

---
+++ basic/parametersOperators.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class ParametersOperators {
+
+
+	  static int myMethod(int x, int y) {
+		    return x + y;
+		  }
+
+		  public static void main(String[] args) {
+		    int z = myMethod(7,8);
+		    System.out.println(z);
+		  }
+		}
+
