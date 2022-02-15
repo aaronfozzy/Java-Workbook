f02c669fbc61ff58b2fd2f64d5160bd022085fea
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-15 11:43:43 +0000

    Create conditionals.java

---
+++ basic/conditionals.java
@@ -0,0 +1,29 @@
+package basic;
+
+public class Conditionals {
+
+
+
+
+		  public static void main(String[] args) {
+			  int x = 20;
+			  int y = 18;
+			  double answer1 = x+y;
+			  double answer2=x*y;
+			  String name = "false";
+			  
+			  if (name == "true") {
+			    System.out.println(answer1);
+			  }
+			    else if(name == "false")
+			    {
+				    System.out.println(answer2);
+
+			    }
+			    else
+			    {
+			    	System.out.println("no answer");
+			    }
+			  }
+		  }
+		
