fca70223ac419c40530b2c5154af28b31d1a5dfc
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-15 11:45:33 +0000

    Create iterationarrays2.java

---
+++ basic/iterationarrays2.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class IteratorArrays2 {
+
+	 public static void main(String[] args) {
+		    int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+		    for (int i = 0; i < numbers.length; i++)
+		    System.out.println(numbers[i]);
+		    
+		    int[] numbers2 = {10,20,30,40,50,60,70,80,90,100};
+		    for (int b = 0; b < numbers2.length; b++)
+		    System.out.println(numbers2[b]);
+		  }
+		}
+
